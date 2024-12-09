c3128bad299a69c4a74540e02a3abeaba88a5f49
lx <github@gmail.com>
2024-12-09 21:12:03 +0800

    Initial commit

---
+++ src/main/java/com/lx/lxmod/LxModClient.java
@@ -0,0 +1,12 @@
+package com.lx.lxmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LxModClient implements ClientModInitializer{
+
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
